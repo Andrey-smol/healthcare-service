e0e15e6356fac95162b95e11b6f164ad3c4722ba
Andrey-smol <andreyksmol@yandex.ru>
2025-03-14 17:47:59 +0300

    reformat code

--- src/test/java/service/medical/MedicalServiceImplTestes.java
+++ src/test/java/service/medical/MedicalServiceImplTestes.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 import ru.netology.patient.entity.BloodPressure;
